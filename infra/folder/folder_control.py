a486388ab1e622d5ff97196f5878614fd4f40702
Brayan <lsouzadiasvaz@gmail.com>
2026-08-03 16:50:34 -0300

    update log

--- infra/folder/folder_control.py
+++ infra/folder/folder_control.py
@@ -1,4 +1,4 @@
-from infra.logs.log import logger
+from infra.logs.log import config_logs
 
 """
 Cria todos os caminhos
@@ -9,7 +9,7 @@ import os
 
 class FolderControl:
 
-    def __init__(self,level:str, path:str = None) -> None:
+    def __init__(self,name:str, level:str, path:str = None) -> None:
 
         #Nivel dos arquivos
         levels = ["processed", "cleaned", "raw"]
@@ -19,22 +19,39 @@ class FolderControl:
         #caminho onde vai salvar tudo, se ele for none, ai vai salvar na raiz do projeto
         self.path = path
 
+        #Nome do projeto
+        self.name = name
+
 
     #Cria o caminho inicial
     def create(self) -> None:
         try:
 
-            if self.path is not None and not os.path.exists(self.path):
+            if not os.path.exists(self.path):
 
-                logger.info(f"Criando o caminho {self.path}...")
+                print(f"Criando o caminho {self.path}...")
                 os.makedirs(self.path, exist_ok=True)
 
         except Exception as e:
             raise Exception(e)
 
+    #Define o caminho
+    def _path(self) -> None:
+            
+            self.path = self.name if self.path is None else f"{self.path}/{self.name}"
+            
+
+    
+
     #Cria o caminho onde os logs vão ser guardados
     def _log(self) -> None:
         try:
+
+            if os.path.exists("app.log"):
+                import shutil
+                shutil.move("app.log", f"{self.path}/logs/app.log")
+            
+
 
 
 
--- infra/logs/log.py
+++ infra/logs/log.py
@@ -4,14 +4,25 @@ configuração global dos logs
 
 import logging
 
-#Configuração basica
-def config_logs(path:str):
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
-        handlers=[
-            logging.FileHandler("app.log"),
-            logging.StreamHandler()
-        ]
-    )
+#Controla os logs
+class Logs:
+    def __init__(self, path:str)->None:
+
+                #Configuração baisca
+                logging.basicConfig(
+                level=logging.INFO,
+                format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+                handlers=[
+                    logging.FileHandler(f"{path}/logs/app.logs"),
+                    logging.StreamHandler()
+                ]
+            )
+
+                #Variavel que carrega a configração
+                self.log = logging.getLogger(__name__)
+
+    #Retorna a variavel que pega a configuração  
+    def logger(self) -> logging:
+            return self.log
+            
 
