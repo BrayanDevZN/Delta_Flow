9db6a0cea6e699cae2d8881a2447863525bd218f
Brayan <lsouzadiasvaz@gmail.com>
2026-08-03 16:16:11 -0300

    debug log

--- infra/folder/folder_control.py
+++ infra/folder/folder_control.py
@@ -1,4 +1,53 @@
+from infra.logs.log import logger
+
 """
 Cria todos os caminhos
 """
+
+import os
+
+
+class FolderControl:
+
+    def __init__(self,level:str, path:str = None) -> None:
+
+        #Nivel dos arquivos
+        levels = ["processed", "cleaned", "raw"]
+        index = levels.index(level)
+        self.level = levels[index:]
+
+        #caminho onde vai salvar tudo, se ele for none, ai vai salvar na raiz do projeto
+        self.path = path
+
+
+    #Cria o caminho inicial
+    def create(self) -> None:
+        try:
+
+            if self.path is not None and not os.path.exists(self.path):
+
+                logger.info(f"Criando o caminho {self.path}...")
+                os.makedirs(self.path, exist_ok=True)
+
+        except Exception as e:
+            raise Exception(e)
+
+    #Cria o caminho onde os logs vão ser guardados
+    def _log(self) -> None:
+        try:
+
+
+
+            
+
+
+
+           
+
+
+
+
+
+
+        
 
--- infra/logs/log.py
+++ infra/logs/log.py
@@ -5,16 +5,13 @@ configuração global dos logs
 import logging
 
 #Configuração basica
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
-    handlers=[
-        logging.FileHandler("app.log"),
-        logging.StreamHandler()
-    ]
-)
-
-#Variavel que pega a conexão
-logger = logging.getLogger(__name__)
-
+def config_logs(path:str):
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+        handlers=[
+            logging.FileHandler("app.log"),
+            logging.StreamHandler()
+        ]
+    )
 
